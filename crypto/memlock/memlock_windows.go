172a0de37ae353ce8de1bf1a70f3355d09e6c31a
Awn <libeclipse@gmail.com>
2017-02-13 16:28:47 +0000

    Have to be careful not to take address if len(b) == 0

--- crypto/memlock/memlock_windows.go
+++ crypto/memlock/memlock_windows.go
@@ -8,14 +8,27 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+// Single-word zero for use when we need a valid pointer to 0 bytes.
+var _zero uintptr
+
 // Lock is a wrapper for windows.VirtualLock()
 func Lock(b []byte) error {
-	p := unsafe.Pointer(&b[0])
-	return windows.VirtualLock(uintptr(p), uintptr(len(b)))
+	var _p0 unsafe.Pointer
+	if len(b) > 0 {
+		_p0 = unsafe.Pointer(&b[0])
+	} else {
+		_p0 = unsafe.Pointer(&_zero)
+	}
+	return windows.VirtualLock(uintptr(_p0), uintptr(len(b)))
 }
 
 // Unlock is a wrapper for windows.VirtualUnlock()
 func Unlock(b []byte) error {
-	p := unsafe.Pointer(&b[0])
-	return windows.VirtualUnlock(uintptr(p), uintptr(len(b)))
+	var _p0 unsafe.Pointer
+	if len(b) > 0 {
+		_p0 = unsafe.Pointer(&b[0])
+	} else {
+		_p0 = unsafe.Pointer(&_zero)
+	}
+	return windows.VirtualUnlock(uintptr(_p0), uintptr(len(b)))
 }
