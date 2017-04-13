8aad2ea837be5ae260d13416f07e3d808dc35462
Awn <libeclipse@gmail.com>
2017-04-13 17:21:14 +0100

    Refactor func TestDerivedIdentifierN() {...}

--- crypto/derivation_test.go
+++ crypto/derivation_test.go
@@ -27,21 +27,15 @@ func TestDeriveSecureValues(t *testing.T) {
 func TestDeriveIdentifierN(t *testing.T) {
 	rootIdentifier, _ := base64.StdEncoding.DecodeString("FIRp7dJQ2RvA7jsQX1DFWxxit6t9ERMyCSloA8iRmU4=")
 
-	// Test when n = 0
-	actualValue, _ := base64.StdEncoding.DecodeString("hZlSQx9fRk9njp7F1NEs+uVowIU/7DcCXZiuW3kAG2g=")
-	if !bytes.Equal(DeriveIdentifierN(rootIdentifier, 0), actualValue) {
-		t.Error("When n=0, derivedIdentifierN != actualValue")
-	}
+	values := []string{
+		"hZlSQx9fRk9njp7F1NEs+uVowIU/7DcCXZiuW3kAG2g=",
+		"PT3TYsQ23cJaxhi250QKdUMVUGGEoddspT9nAeSFoj0=",
+		"jKNwKnosCmInggyaqFX/OWehVjtWIywjiyTvgCRZ+T8="}
 
-	// Test when n = 1
-	actualValue, _ = base64.StdEncoding.DecodeString("PT3TYsQ23cJaxhi250QKdUMVUGGEoddspT9nAeSFoj0=")
-	if !bytes.Equal(DeriveIdentifierN(rootIdentifier, 1), actualValue) {
-		t.Error("When n=1, derivedIdentifierN != actualValue")
-	}
-
-	// Test when n = 2
-	actualValue, _ = base64.StdEncoding.DecodeString("jKNwKnosCmInggyaqFX/OWehVjtWIywjiyTvgCRZ+T8=")
-	if !bytes.Equal(DeriveIdentifierN(rootIdentifier, 2), actualValue) {
-		t.Error("When n=2, derivedIdentifierN != actualValue")
+	for i, v := range values {
+		actualValue, _ := base64.StdEncoding.DecodeString(v)
+		if !bytes.Equal(DeriveIdentifierN(rootIdentifier, i), actualValue) {
+			t.Errorf("When n=%d, derivedIdentifierN != actualValue", i)
+		}
 	}
 }
